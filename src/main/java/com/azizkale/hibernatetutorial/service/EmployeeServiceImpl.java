429694b43bfb39ca75725a27374f9375f45a1283
azizkale <azizkale@hotmail.com>
2023-08-20 11:50:01 +0200

    Exceptions classes added

--- src/main/java/com/azizkale/hibernatetutorial/service/EmployeeServiceImpl.java
+++ src/main/java/com/azizkale/hibernatetutorial/service/EmployeeServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
